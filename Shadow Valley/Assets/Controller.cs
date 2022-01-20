bd51b9da02d011dc0dddd065d8c658ee10240362
Lucas Bender <lucasbenders@gmail.com>
2022-01-20 19:58:38 -0300

    First script and tests

---
+++ Shadow Valley/Assets/Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public bool isJumping = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
