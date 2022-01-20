7a5b77e5e7d507de155c23f3c158e09c81e623e0
Lucas Bender <lucasbenders@gmail.com>
2022-01-20 20:58:04 -0300

    Fix movement and add Jump to Player

--- Shadow Valley/Assets/Scripts/Controller.cs
+++ Shadow Valley/Assets/Scripts/Controller.cs
@@ -6,10 +6,27 @@ public class Controller : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public bool isGrounded = true;
+    Vector3 jump;
+    public float jumpForce = 2.0f;
+    bool isGrounded;
+    Rigidbody rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0, jumpForce, 0);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(){
+        isGrounded = false;
+    }
 
     void Update()
     {
+
         float horizontalInput = Input.GetAxis("Horizontal");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, 0);
@@ -22,5 +39,13 @@ public class Controller : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
         }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            Jump();
+        }
+    }
+
+    void Jump(){
+        rb.AddForce(jump * jumpForce, ForceMode.Impulse);
     }
 }
