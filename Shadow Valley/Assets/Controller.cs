ef1b20419975144c72b33f73ac411fcb41cd8f07
Lucas Bender <lucasbenders@gmail.com>
2022-01-20 20:24:50 -0300

    Add simple 2d movement to character

--- Shadow Valley/Assets/Controller.cs
+++ Shadow Valley/Assets/Controller.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public bool isJumping = false;
-
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float rotationSpeed;
+    public bool isGrounded = true;
 
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, 0);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
         
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
+        }
     }
 }
